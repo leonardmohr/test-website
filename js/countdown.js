4c9411b07ba5182fcd749a05af2500c5c1247b70
leonardmohr <64943444+leonardmohr@users.noreply.github.com>
2023-10-08 12:57:59 -0600

    Updated showcase

--- js/countdown.js
+++ js/countdown.js
@@ -1,173 +1,30 @@
-const now = new Date();
-const weddingTime = new Date("2025-03-05");
-const timeRemaining = weddingTime - now;
+var end = new Date('03/05/2025 10:1 AM');
 
-const daysRemaining =  Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-const hoursRemaining =  Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-const minutesRemaining = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-const secondsRemaining = Math.floor((timeRemaining % (1000 * 60)) / (1000));
+var _second = 1000;
+var _minute = _second * 60;
+var _hour = _minute * 60;
+var _day = _hour * 24;
+var timer;
 
-// Create Countdown
-var Countdown = {
-  
-  /* The variable $el will reference all DOM element(s) with class 'countdown' */ 
-  $el: $('.countdown'),
-  
-  // Params
-  countdown_interval: null,
-  total_seconds: 0,
-  
-  /* 'init' is a property of the 'Countdown' object, that when called will initialize
-      the countdown timer */
-  init: function() {
-    
-    /* The '$' by convention is used to indicate that the properties
-        of this object will hold jquery objects */ 
-		this.$ = {
-      /* finds all elements with class figure that are nested
-         under an element with both block-time and hours classes. */
-      days: this.$el.find('.bloc-time.days .figure'),
-    	hours: this.$el.find('.bloc-time.hours .figure'),
-    	minutes: this.$el.find('.bloc-time.min .figure'),
-    	seconds: this.$el.find('.bloc-time.sec .figure')
-   	};
+function showRemaining() {
+    var now = new Date();
+    var distance = end - now;
+    if (distance < 0) {
 
+        clearInterval(timer);
+        document.getElementById('countdown').innerHTML = 'EXPIRED!';
 
-
-    // Init countdown values
-    this.values = {
-        days: daysRemaining,
-	      hours: hoursRemaining,
-        minutes: minutesRemaining,
-        seconds: secondsRemaining,
-    };
-    
-    // Initialize total seconds
-    this.total_seconds = (this.values.days * 60*60*24 )+ (this.values.hours * 60 * 60) + (this.values.minutes * 60) + (this.values.seconds);
-
-    // Animate countdown to the end 
-    this.count();    
-  },
-  
-  count: function() {
-    
-    /* The eq function just selects the element in collection with
-        specific index.  for example this.$.hours.eq(0) selects the 
-        first object contained in hours.  This is determined by its
-        place in the DOM. */
-    var that    = this,
-        $day_1 = this.$.days.eq(0),
-        $day_2 = this.$.days.eq(1),
-        $day_3 = this.$.days.eq(2),
-        $hour_1 = this.$.hours.eq(0),
-        $hour_2 = this.$.hours.eq(1),
-        $min_1  = this.$.minutes.eq(0),
-        $min_2  = this.$.minutes.eq(1),
-        $sec_1  = this.$.seconds.eq(0),
-        $sec_2  = this.$.seconds.eq(1);
-    
-        this.countdown_interval = setInterval(function() {
-
-        if(that.total_seconds > 0) {
-
-            --that.values.seconds;              
-
-            if(that.values.minutes >= 0 && that.values.seconds < 0) {
-
-                that.values.seconds = 59;
-                --that.values.minutes;
-            }
-
-            if(that.values.hours >= 0 && that.values.minutes < 0) {
-
-                that.values.minutes = 59;
-                --that.values.hours;
-            }
-
-            if(that.values.days >= 0 && that.values.hours < 0) {
-              that.values.hours = 59;
-              --that.values.days;
-            }
-
-            // Update DOM values
-            // Days
-            that.checkHour(that.values.days, $day_1, $day_2, $day_3);
-
-            // Hours
-            that.checkHour(that.values.hours, $hour_1, $hour_2);
-
-            // Minutes
-            that.checkHour(that.values.minutes, $min_1, $min_2);
-
-            // Seconds
-            that.checkHour(that.values.seconds, $sec_1, $sec_2);
-
-            --that.total_seconds;
-        }
-        else {
-            clearInterval(that.countdown_interval);
-        }
-    }, 1000);    
-  },
-  
-  animateFigure: function($el, value) {
-    
-     var that         = this,
-		     $top         = $el.find('.top'),
-         $bottom      = $el.find('.bottom'),
-         $back_top    = $el.find('.top-back'),
-         $back_bottom = $el.find('.bottom-back');
-
-    // Before we begin, change the back value
-    $back_top.find('span').html(value);
-
-    // Also change the back bottom value
-    $back_bottom.find('span').html(value);
-
-    // Then animate
-    TweenMax.to($top, 0.8, {
-        rotationX           : '-180deg',
-        transformPerspective: 300,
-	      ease                : Quart.easeOut,
-        onComplete          : function() {
-
-            $top.html(value);
-
-            $bottom.html(value);
-
-            TweenMax.set($top, { rotationX: 0 });
-        }
-    });
-
-    TweenMax.to($back_top, 0.8, { 
-        rotationX           : 0,
-        transformPerspective: 300,
-	      ease                : Quart.easeOut, 
-        clearProps          : 'all' 
-    });    
-  },
-  
-  checkHour: function(value, $el_1, $el_2) {
-    
-    var val_1       = value.toString().charAt(0),
-        val_2       = value.toString().charAt(1),
-        fig_1_value = $el_1.find('.top').html(),
-        fig_2_value = $el_2.find('.top').html();
-
-    if(value >= 10) {
-
-        // Animate only if the figure has changed
-        if(fig_1_value !== val_1) this.animateFigure($el_1, val_1);
-        if(fig_2_value !== val_2) this.animateFigure($el_2, val_2);
+        return;
     }
-    else {
+    var days = Math.floor(distance / _day);
+    var hours = Math.floor((distance % _day) / _hour);
+    var minutes = Math.floor((distance % _hour) / _minute);
+    var seconds = Math.floor((distance % _minute) / _second);
 
-        // If we are under 10, replace first figure with 0
-        if(fig_1_value !== '0') this.animateFigure($el_1, 0);
-        if(fig_2_value !== val_1) this.animateFigure($el_2, val_1);
-    }    
-  }
-};
+    document.getElementById('days').innerHTML = days;
+    document.getElementById('hours').innerHTML = hours;
+    document.getElementById('minutes').innerHTML = minutes;
+    document.getElementById('seconds').innerHTML = seconds;
+}
 
-// Let's go !
-Countdown.init();
+timer = setInterval(showRemaining, 1000);
